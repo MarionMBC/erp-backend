504492fc57c8dd7a13472bd3a63b11fc1756aff7
iamchapita <alejandrom646@gmail.com>
2023-06-21 01:38:11 -0600

    Obtención de credenciales desde .env

--- config/database.js
+++ config/database.js
@@ -1,15 +1,16 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv'
+import mysql from "mysql2";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 // Configuración de la conexión a la base de datos
 const dbConfig = {
-    host: process.env.HOST,
-    user: 'root',
-    password: 'tucontraseña',
-    database: 'nombre_basedatos',
-    connectionLimit: 10 // Número máximo de conexiones en el pool
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASSWORD,
+	port: process.env.DATABASEPORT,
+	database: process.env.DATABASE,
+	connectionLimit: 10, // Número máximo de conexiones en el pool
 };
 
 // Crear el pool de conexiones
@@ -17,31 +18,28 @@ const pool = mysql.createPool(dbConfig);
 
 // Obtener una conexión del pool
 pool.getConnection((err, connection) => {
-    if (err) {
-        console.error('Error al obtener una conexión del pool:', err);
-        return;
-    }
+	if (err) {
+		console.error("Error al obtener una conexión del pool:", err);
+		return;
+	}
 
-    // Realizar consultas utilizando la conexión
+	// Realizar consultas utilizando la conexión
 
-    // ...
+	// ...
 
-    // Devolver la conexión al pool cuando haya terminado de usarla
-    connection.release();
+	// Devolver la conexión al pool cuando haya terminado de usarla
+	connection.release();
 });
 
 // Ejemplo de consulta utilizando el pool
-pool.query('SELECT * FROM usuarios', (err, results) => {
-    if (err) {
-        console.error('Error al ejecutar la consulta:', err);
-        return;
-    }
+pool.query("SELECT * FROM usuarios", (err, results) => {
+	if (err) {
+		console.error("Error al ejecutar la consulta:", err);
+		return;
+	}
 
-    console.log('Resultados:', results);
+	console.log("Resultados:", results);
+	return;
 });
 
-
-
-
-
-
+export default pool

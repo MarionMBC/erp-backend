db5d78dc0bd360057b46cee2573cb5ea4229eed5
iamchapita <alejandrom646@gmail.com>
2023-06-21 01:38:34 -0600

    Camibio de ruta base

--- config/server.js
+++ config/server.js
@@ -2,20 +2,17 @@ import express from "express";
 import router from "../routes/index.js";
 const app = express();
 
-
 //MiddleWare
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static('public'))
-app.use(router)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(router);
 
-
-app.get('/', (req, res) => {
-    res.status(200).json({
-        status: 200,
-        msg: "Bienvenido al servidor de SyncPro."
-    })
-})
-
+app.get("/syncpro/", (req, res) => {
+	res.status(200).json({
+		status: 200,
+		msg: "Bienvenido al servidor de SyncPro.",
+	});
+});
 
 export default app;

a6c3fdf0af91341545b862dc5e1cba359d30c075
iamchapita <alejandrom646@gmail.com>
2023-06-28 13:51:20 -0600

    Implementacion de casteo de campos tipo Datetime en parametros de conexion

--- app/config/database.js
+++ app/config/database.js
@@ -1,19 +1,26 @@
 import mysql from "mysql2/promise.js";
 import dotenv from "dotenv";
+import moment from "moment";
 
 dotenv.config();
 
 // Configuración de la conexión a la base de datos
 const dbConfig = {
 	host: process.env.HOST,
-	user: process.env.USER,
+	user: "sync", // NO CAMBIAR
 	password: process.env.PASSWORD,
 	port: process.env.DATABASEPORT,
 	database: process.env.DATABASE,
-	connectionLimit: 10, // Número máximo de conexiones en el pool
+	connectionLimit: 10,
+	typeCast: function (field, next) {
+		if (field.type === "DATETIME") {
+			return moment(field.string()).format("YYYY-MM-DD hh:mm:ss A");
+		}
+		return next();
+	},
 };
 
 // Crear el pool de conexiones
 const pool = mysql.createPool(dbConfig);
 
-export default pool
+export default pool;

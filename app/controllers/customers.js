4d8174afca8b01e36aa3d5196ab88473e6110f9f
iamchapita <alejandrom646@gmail.com>
2023-06-28 21:53:20 -0600

    Se agregaron campos a la consulta SQL

--- app/controllers/customers.js
+++ app/controllers/customers.js
@@ -31,19 +31,39 @@ const getCustomers = async (request, response) => {
 const getCustomerById = async (request, response) => {
 	const query = `
 		SELECT 
-			customers.id as id,
+			customers.id AS id,
 			customers.firstNames AS customerFirstName,
 			customers.lastNames AS customerLastName,
 			customers.country AS customerCountry,
 			customers.city AS customerCity,
 			customers.createdAt AS customerAddedAt,
-			customerType.name AS customerType
+			customerContactInfo.email as customerEmail,
+			customerContactInfo.phoneNumber as customerPhoneNumber,
+			customerType.name AS customerType,
+			naturalCustomerTypeDetails.rtn AS naturalCustomerRTN,
+			businessCustomerTypeDetails.businessName AS businessCustomerName,
+			businessCustomerTypeDetails.rtn AS businessCustomerRTN,
+			businessCustomerTypeDetails.hasCredit AS businessCustomerHasCredit,
+			businessCustomerTypeDetails.creditAmount AS businessCustomerCreditAmount,
+			customers.createdAt AS customerRegisteredAt
 		FROM 
 			customers
 		JOIN
 			customerType
 		ON
 			customers.id = customerType.idCustomerFK
+		JOIN 
+			customerContactInfo
+		ON
+			customers.id = customerContactInfo.idCustomerFK
+		LEFT JOIN
+			naturalCustomerTypeDetails	
+		ON
+			customerType.id = naturalCustomerTypeDetails.idCustomerTypeFK
+		LEFT JOIN
+			businessCustomerTypeDetails	
+		ON
+			customerType.id = businessCustomerTypeDetails.idCustomerTypeFK
 		wHERE
 			customers.id = ?
 	`;

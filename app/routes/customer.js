b8c759e29f7b45aca338c049eee173b02b1aacf3
iamchapita <alejandrom646@gmail.com>
2023-06-30 23:44:50 -0600

    Implementacion de rutas y organizacion de las mismas por tabla a afectar

--- app/routes/customer.js
+++ app/routes/customer.js
@@ -12,19 +12,46 @@ import {
 	updateCustomerTypeByCustomerId,
 } from "../controllers/customer/customerType.js";
 
+import {
+	addBusinessCustomerDetails,
+	updateBusinessCustomerDetails,
+	updateBusinessCustomerDetailsByCustomerId,
+} from "../controllers/customer/businessCustomerTypeDetails.js";
+
+import {
+	addNaturalCustomerDetails,
+	updateNaturalCustomerDetails,
+	updateNaturalCustomerDetailsByCustomerId,
+} from "../controllers/customer/naturalCustomerTypeDetails.js";
+
 const router = express.Router();
 
-// GET
+// =====================================CUSTOMERS==========================================
 router.get("/getCustomers", getCustomers);
 router.get("/getCustomerById", getCustomerById);
-
-// POST
 router.post("/addCustomer", addCustomer);
-router.post("/addCustomerType", addCustomerType);
-
-// PUT
 router.put("/updateCustomer", updateCustomer);
+
+// ===================================CUSTOMERTYPE==========================================
+router.post("/addCustomerType", addCustomerType);
+router.post("/addCustomerType", addCustomerType);
 router.put("/updateCustomerType", updateCustomerType);
 router.put("/updateCustomerTypeByCustomerId", updateCustomerTypeByCustomerId);
+
+// ============================BUSINESSCUSTOMERTYPEDETAILS==================================
+router.post("/addBusinessCustomerDetails", addBusinessCustomerDetails);
+router.put("/updateBusinessCustomerDetails", updateBusinessCustomerDetails);
+router.put(
+	"/updateBusinessCustomerDetailsByCustomerId",
+	updateBusinessCustomerDetailsByCustomerId
+);
+
+// ==============================NATURALCUSTOMERTYPEDETAILS=================================
+router.post("/addNaturalCustomerDetails", addNaturalCustomerDetails);
+router.put("/updateNaturalCustomerDetails", updateNaturalCustomerDetails);
+router.put(
+	"/updateNaturalCustomerDetailsByCustomerId",
+	updateNaturalCustomerDetailsByCustomerId
+);
 
 export { router };

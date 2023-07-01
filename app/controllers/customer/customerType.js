aa21b726404c369f68777370f50a94f282c024f9
iamchapita <alejandrom646@gmail.com>
2023-06-30 21:43:26 -0600

    Endpoints para customerType (insercion y actualizacion)

--- app/controllers/customer/customerType.js
+++ app/controllers/customer/customerType.js
@@ -17,4 +17,43 @@ const addCustomerType = async (request, response) => {
 	}
 };
 
-export { addCustomerType };
+const updateCustomerType = async (request, response) => {
+	const { id, idCustomerFK, name } = request.body;
+	const query = `
+        UPDATE 
+			customerType 
+		SET 
+			idCustomerFK = ?, 
+			name = ?,
+			updatedAt = CURRENT_TIMESTAMP
+		WHERE
+			id = ?`;
+
+	try {
+		const [result] = await pool.execute(query, [idCustomerFK, name, id]);
+		response.status(200).json(result);
+	} catch (e) {
+		response.status(500).json(e);
+	}
+};
+
+const updateCustomerTypeByCustomerId = async (request, response) => {
+	const { idCustomerFK, name } = request.body;
+	const query = `
+        UPDATE 
+			customerType 
+		SET 
+			name = ?,
+			updatedAt = CURRENT_TIMESTAMP
+		WHERE
+			idCustomerFK = ?`;
+
+	try {
+		const [result] = await pool.execute(query, [name, idCustomerFK]);
+		response.status(200).json(result);
+	} catch (e) {
+		response.status(500).json(e);
+	}
+};
+
+export { addCustomerType, updateCustomerType, updateCustomerTypeByCustomerId };
--- app/routes/customer.js
+++ app/routes/customer.js
@@ -6,14 +6,25 @@ import {
 	updateCustomer,
 } from "../controllers/customer/customer.js";
 
-import { addCustomerType } from "../controllers/customer/customerType.js";
+import {
+	addCustomerType,
+	updateCustomerType,
+	updateCustomerTypeByCustomerId,
+} from "../controllers/customer/customerType.js";
 
 const router = express.Router();
 
+// GET
 router.get("/getCustomers", getCustomers);
 router.get("/getCustomerById", getCustomerById);
+
+// POST
 router.post("/addCustomer", addCustomer);
 router.post("/addCustomerType", addCustomerType);
+
+// PUT
 router.put("/updateCustomer", updateCustomer);
+router.put("/updateCustomerType", updateCustomerType);
+router.put("/updateCustomerTypeByCustomerId", updateCustomerTypeByCustomerId);
 
 export { router };

506157abb29af5011863af37dfeae2a67bb87f7e
iamchapita <alejandrom646@gmail.com>
2023-08-20 19:15:29 -0600

    Implementacion de endpoints

--- app/routes/sales.js
+++ app/routes/sales.js
@@ -1,10 +1,23 @@
 import express from "express";
 
-import { getPurchaseOrderProducts,addProductToPurchaseOrder } from "../controllers/sales/purchaseOrderProducts.js";
+import {
+	getPurchaseOrderProducts,
+	addProductToPurchaseOrder,
+} from "../controllers/sales/purchaseOrderProducts.js";
+import {
+	getPurchaseOrders,
+	getPurchaseOrderById,
+	addPurchaseOrder,
+} from "../controllers/sales/purchaseOrder.js";
 
 const router = express.Router();
 
-// =====================================CUSTOMERS===========================================
+// =====================================PURCHASEORDER===================================================
+router.get("/getPurchaseOrders", getPurchaseOrders);
+router.post("/getPurchaseOrderById", getPurchaseOrderById);
+router.post("/addPurchaseOrder", addPurchaseOrder);
+
+// =====================================PURCHASEORDERPRODUCTS===========================================
 router.get("/getPurchaseOrderProducts", getPurchaseOrderProducts);
 router.post("/addProductToPurchaseOrder", addProductToPurchaseOrder);
 // router.post("/addCustomer", addCustomer);
